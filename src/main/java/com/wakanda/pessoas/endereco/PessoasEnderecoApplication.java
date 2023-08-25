f0aa0447fba0a5f14220cc43b2132a5f3d6db9c4
MARCIO MOURA DOS SANTOS <113490711+Marcioavb@users.noreply.github.com>
2023-08-25 09:20:06 -0300

    inicio projeto

---
+++ src/main/java/com/wakanda/pessoas/endereco/PessoasEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.pessoas.endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakanda/pessoas/endereco/PessoasEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda.pessoas.endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
