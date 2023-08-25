f65de0e0992262e36a2cdcac895f81103b07b920
MARCIO MOURA DOS SANTOS <113490711+Marcioavb@users.noreply.github.com>
2023-08-25 09:27:53 -0300

    mapeia a classe principal, renomeia o application propertis e cria as entidades pessoas, endreco e handler

--- src/main/java/com/wakanda/pessoas/endereco/PessoasEnderecoApplication.java
+++ src/main/java/com/wakanda/pessoas/endereco/PessoasEnderecoApplication.java
@@ -2,8 +2,13 @@ package com.wakanda.pessoas.endereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
+
 public class PessoasEnderecoApplication {
 
 	public static void main(String[] args) {
